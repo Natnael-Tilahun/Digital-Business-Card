4a03f6e0056b4c8c84f746591428e4178797d383
natnael-tilahun <natnaeltilahun97@gmail.com>
2022-08-22 11:24:53 +0300

    Change image size and font type

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default function Footer(){
     return(
         <div className="footer-div">
-            <a href=""><i className="fa fa-twitter"></i></a>
-            <a href=""><i className="fa fa-facebook"></i></a>
-            <a href=""><i className="fa fa-instagram"></i></a>
-            <a href="https://github.com/Natnael-Tilahun"><i className="fa fa-github"></i></a>
+            <a href="#"><i className="fa fa-twitter"></i></a>
+            <a href="#"><i className="fa fa-facebook"></i></a>
+            <a href="#"><i className="fa fa-instagram"></i></a>
+            <a href="https://github.com/Natnael-Tilahun" target="_blank"><i className="fa fa-github"></i></a>
         </div>
     )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -10,11 +10,11 @@ export default function Info(){
             <p className="info-website">NhattyTech.com</p>
             <div className="info-btns">
                 <button className="email-btn">
-                    <a href="mailto:natnaeltilahun97@gmail.com" className="email-link"><i className="fa fa-envelope"  style={{paddingRight: "10px"}}></i>Email
+                    <a href="mailto:natnaeltilahun97@gmail.com" className="email-link" target="_blank"><i className="fa fa-envelope"  style={{paddingRight: "10px"}}></i>Email
                     </a>
                 </button>
                 <button className="linkedin-btn">
-                    <a href="https://www.linkedin.com/in/natnael-tilahun/" className="linkedin-link" target=""><i className="fa fa-linkedin" style={{marginRight: "8px",background:"white", color:'#5093E2', padding:'1.2px', borderRadius:'2px'}}></i>LinkedIn
+                    <a href="https://www.linkedin.com/in/natnael-tilahun/" className="linkedin-link" target="_blank"><i className="fa fa-linkedin" style={{marginRight: "8px",background:"white", color:'#5093E2', padding:'1.2px', borderRadius:'2px'}}></i>LinkedIn
                     </a>
                 </button>
             </div>
